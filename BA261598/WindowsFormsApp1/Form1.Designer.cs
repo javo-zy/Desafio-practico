cde09ea1fb6925224ac8525bb13fb7b94182cb7a
javo-zy <168499564+javo-zy@users.noreply.github.com>
2026-02-26 00:34:54 -0600

    Add files via upload

---
+++ BA261598/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPrecioOriginal = new System.Windows.Forms.TextBox();
+            this.txtMontoTotal = new System.Windows.Forms.TextBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.lblPrecioFinal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPrecioOriginal
+            // 
+            this.txtPrecioOriginal.Location = new System.Drawing.Point(247, 149);
+            this.txtPrecioOriginal.Name = "txtPrecioOriginal";
+            this.txtPrecioOriginal.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecioOriginal.TabIndex = 0;
+            // 
+            // txtMontoTotal
+            // 
+            this.txtMontoTotal.Location = new System.Drawing.Point(353, 149);
+            this.txtMontoTotal.Name = "txtMontoTotal";
+            this.txtMontoTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtMontoTotal.TabIndex = 1;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Items.AddRange(new object[] {
+            "Electrónica",
+            "Ropa",
+            "Alimentos",
+            "Hogar"});
+            this.cmbCategoria.Location = new System.Drawing.Point(247, 186);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategoria.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(378, 186);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(286, 230);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(104, 13);
+            this.lblMensaje.TabIndex = 4;
+            this.lblMensaje.Text = "Esperando cálculo...";
+            // 
+            // lblPrecioFinal
+            // 
+            this.lblPrecioFinal.AutoSize = true;
+            this.lblPrecioFinal.Location = new System.Drawing.Point(286, 252);
+            this.lblPrecioFinal.Name = "lblPrecioFinal";
+            this.lblPrecioFinal.Size = new System.Drawing.Size(92, 13);
+            this.lblPrecioFinal.TabIndex = 5;
+            this.lblPrecioFinal.Text = "Precio final: $0.00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Categoría";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Monto total";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Precio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPrecioFinal);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.txtMontoTotal);
+            this.Controls.Add(this.txtPrecioOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPrecioOriginal;
+        private System.Windows.Forms.TextBox txtMontoTotal;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Label lblPrecioFinal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BA261598/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Leer lo que el usuario escribió y guardarlo en variables
+                double precioOriginal = Convert.ToDouble(txtPrecioOriginal.Text);
+                double montoTotal = Convert.ToDouble(txtMontoTotal.Text);
+                string categoria = cmbCategoria.Text;
+
+                // Variable para guardar el descuento 
+                double descuento = 0;
+
+                // Evaluar la categoría usando switchcase 
+                switch (categoria)
+                {
+                    case "Electrónica":
+                        if (montoTotal > 500)
+                        {
+                            descuento = 0.10; // 10%
+                        }
+                        break;
+
+                    case "Ropa":
+                        if (montoTotal > 300)
+                        {
+                            descuento = 0.15; // 15%
+                        }
+                        break;
+
+                    case "Alimentos":
+                        if (montoTotal > 200)
+                        {
+                            descuento = 0.05; // 5%
+                        }
+                        break;
+
+                    case "Hogar":
+                        if (montoTotal > 400)
+                        {
+                            descuento = 0.20; // 20%
+                        }
+                        break;
+
+                    default:
+                        // Si no seleccionó nada en el ComboBox
+                        MessageBox.Show("Por favor, seleccione una categoría.");
+                        return; // Salimos de aquí para que no calcule nada
+                }
+
+                // Hacer las operaciones matemáticas básicas
+                double cantidadDescontada = precioOriginal * descuento;
+                double precioFinal = precioOriginal - cantidadDescontada;
+
+                // Mostrar el precio en el Label
+                lblPrecioFinal.Text = "Precio final: $" + precioFinal;
+
+                if (descuento > 0)
+                {
+                    // Multiplicamos por 100 para que se vea como porcentaje
+                    double porcentaje = descuento * 100;
+                    lblMensaje.Text = "Se aplicó un descuento del " + porcentaje + "%.";
+                }
+                else
+                {
+                    lblMensaje.Text = "No se aplicó ningún descuento.";
+                }
+            }
+            catch
+            {
+                // Si hay un error 
+                MessageBox.Show("Error: Asegúrese de escribir solo números válidos en las cajas de texto.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
